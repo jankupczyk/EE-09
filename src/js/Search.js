a3bb9b931359109cb8ab98191468805871bf87e3
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2022-02-01 13:16:23 +0100

    Add files via upload

--- src/js/Search.js
+++ src/js/Search.js
@@ -2,12 +2,12 @@ document.getElementById("inputed").addEventListener("keyup", (e) => {
     let searchQuery = e.target.value.toLowerCase();
     let rows = document.querySelectorAll('tr');
 
-    for (i = 1; i < rows.length; i++) {
-        if (!rows[i].childNodes[2].textContent.toLowerCase().includes(searchQuery)) {
-            rows[i].style.display = "none";
+    Array.from(rows).slice(1).forEach(row => {
+        if (!row.childNodes[2].textContent.toLowerCase().includes(searchQuery)) {
+            row.style.display = "none";
         }
         else {
-            rows[i].style.display = "table-row";
-        }
-    }
+            row.style.display = "table-row";
+        }        
+    });
 });
